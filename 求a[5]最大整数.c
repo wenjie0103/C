ae246a403bec7f0574c1bb0e6b624e7c949cd468
wenjie0103 <161571631+wenjie0103@users.noreply.github.com>
2024-02-28 20:11:13 +0800

    Add files via upload

---
+++ 求a[5]最大整数.c
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+int main()
+{
+	int i, max = 0, a[5] = { 0,10,20,30,40 };
+	int* pmax, * pa;
+	pa = pmax = a;
+	for (i = 1, i < 5, i++);
+	if (a[max] > a[i])max = i;
+	printf("最大值是%d\n", a[max]);
+	for (i = 0, i < 5; i++; );
+	{	if (*pmax < *pa)pmax = pa;
+	pa++;
+	}
+	printf("最大值是%d\n", *pmax);
+	return 0;
+}
